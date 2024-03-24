7366dbe0fac503fea773662874b114b43ccab3fb
⚔ DEXTER ⚔ <143838400+DEXTER-BOT-1@users.noreply.github.com>
2024-03-24 18:50:52 +0530

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello This is  *FLASH-MD* \n\n ' + "The Following is *FLASH-MD Repo.*";
-    let d = ' https://github.com/franceking1/Flash-Md';
+    let z = 'Hello This is  *DEXTER-MD* \n\n ' + "The Following is *DEXTER-MD Repo.*";
+    let d = ' https://github.com/DEXTER-BOT-1/DEXTER-V1-MD';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    var img = 'https://telegra.ph/file/57c3083b60b2bea6ec132.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *France King*'
+   let z ='Salut je m\'appelle *DEXTER-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Dexter*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
